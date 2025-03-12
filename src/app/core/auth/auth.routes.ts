4672dde59dbe5dd12e475c9806c561c3a5a62abd
Royer Tzep <rtzepi@miumg.edu.gt>
2025-03-12 15:41:19 -0600

    login comienzo

---
+++ src/app/core/auth/auth.routes.ts
@@ -0,0 +1,6 @@
+// import { Routes } from '@angular/router';
+// import { LoginComponent } from './pages/login/login.component';
+
+// export const AuthRoutes: Routes = [
+//   { path: 'login', component: LoginComponent }
+// ];
